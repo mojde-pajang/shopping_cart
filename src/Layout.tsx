61c0a4bd6a14ef2906cb898edfd50185fcee76fb
Mojdeh Pajang <mojde.pajang@gmail.com>
2024-07-22 12:06:47 +0200

    Do the navigation menu

---
+++ src/Layout.tsx
@@ -0,0 +1,41 @@
+import { NavLink, Outlet } from 'react-router-dom'
+import { ShoppingCart } from 'lucide-react';
+import { Button } from './components/ui/button';
+
+function linkClasses(isActive: boolean) {
+  const classes = "block  text-base md:text-base px-5 py-1.5 rounded transition-colors ";
+  const conditionalClasses = isActive ? " text-white bg-green-800 " : " hover:text-green-700 ";
+  return classes + conditionalClasses
+
+}
+function Layout() {
+  return (
+    <div className="flex min-h-screen w-full flex-col">
+      <header className="sticky top-0 flex h-16 items-center gap-4 justify-between border-b bg-background px-4 md:px-6">
+        <nav className="hidden flex-col gap-2 text-lg font-medium md:flex md:flex-row md:items-center  md:text-sm ">
+          <NavLink to="/"
+            className={({ isActive }) => linkClasses(isActive)}>
+            Home
+          </NavLink>
+          <NavLink to="/shop"
+            className={({ isActive }) => linkClasses(isActive)}>
+            Shop
+          </NavLink>
+          <NavLink to="/about"
+            className={({ isActive }) => linkClasses(isActive)}>
+            About
+          </NavLink>
+        </nav>
+        <Button variant="outline" size="icon" className=' p-2 relative rounded-full ' >
+          <ShoppingCart color='#15803d' />
+          <span className=' absolute bg-green-800 rounded-full w-5 h-5 text-white right-0 bottom-0 translate-y-2 translate-x-2	'>3</span>
+        </Button>
+      </header>
+      <body>
+        <Outlet />
+      </body>
+    </div>
+  )
+}
+
+export default Layout
--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,12 +1,13 @@
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom";
 import { About, Home, Shop } from "./pages";
+import Layout from "./Layout";
 
 
 function Routes() {
 
   const router = createBrowserRouter(createRoutesFromElements(
-    <Route>
-      <Route path="/" element={<Home />} />,
+    <Route path="/" element={<Layout />}>
+      <Route index element={<Home />} />,
       <Route path="/shop" element={<Shop />} />,
       <Route path="/about" element={<About />} />,
       <Route path="*" element={(<div> Not Found</div>)} />
